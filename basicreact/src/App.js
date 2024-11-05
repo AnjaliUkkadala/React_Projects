7f37b8aac50e52bc725ab0dc3f3a5bb6c8cd5145
AnjaliUkkadala <anjaliukkadala796@gmail.com>
2024-11-05 11:36:23 +0530

    Create App.js

---
+++ basicreact/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+//import './App.css';
+import Youtube from './Youtube';
+
+function App() {
+  return (
+    <div>
+        <h1> Classic react App</h1>
+        <Youtube />
+    </div>
+
+  );
+}
+
+export default App;
